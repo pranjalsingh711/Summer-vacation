5bdddbaf634ae5648009e738f0469a40ae9e2274
pranjalsingh711 <pranjalsingh7112006@gmail.com>
2026-06-13 11:45:40 +0530

    code done

---
+++ summer_vacation/day_13/Q52.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i;
+
+    int even = 0, odd = 0;
+
+    printf("Enter size of array: ");
+
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even elements = %d\n", even);
+    
+    printf("Odd elements = %d", odd);
+
+    return 0;
+}
