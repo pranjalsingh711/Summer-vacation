d282e2023d0636fc8887b8cab40f0d180b7fabdf
pranjalsingh711 <pranjalsingh7112006@gmail.com>
2026-06-21 22:49:31 +0530

    code done

--- practice/kj.cpp
+++ practice/kj.cpp
@@ -1,19 +1,18 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
-int main()
-{
+int main (){
   int n;
-  cout<<"enter "<<endl;
+  cout<<"enter the number"<<endl;
   cin>>n;
-  int row=1;
-  
- 
- 
-    
-      
-   return 0;
-    }
-
- 
- 
-
+  int ans =0;
+  int i=0;
+  while(n!=0){
+    int bit=n&1;
+   ans=ans+(bit*pow(10,i));
+   n=n>>1;
+   i++;
+  }
+  cout<<"the binary no.is "<<ans<<endl;
+  return 0;
+}
