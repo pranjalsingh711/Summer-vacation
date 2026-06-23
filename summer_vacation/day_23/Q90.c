516e12849a778320a889079b55a25751265568d6
pranjalsingh711 <pranjalsingh7112006@gmail.com>
2026-06-23 22:10:37 +0530

    code done

---
+++ summer_vacation/day_23/Q90.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    char str[1000];
+    int freq[256] = {0};
+
+
+    printf("Enter a string: ");
+
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+
+        if (freq[(unsigned char)str[i]] == 1) {
+
+            printf("First repeating character: %c\n", str[i]);
+            
+            return 0;
+        }
+        freq[(unsigned char)str[i]]++;
+    }
+
+    printf("No repeating character found.\n");
+
+    return 0;
+}
